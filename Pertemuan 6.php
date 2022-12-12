9d4f0139deb5d2a395c08dd7d04acaa8afe7224f
manarulh <65214965+manarulh@users.noreply.github.com>
2022-12-12 11:36:14 +0700

    Add files via upload

---
+++ Pertemuan 6.php
@@ -0,0 +1,34 @@
+<?php
+function intTOroman($number)
+{
+	 $number = intval($number);
+	 $result = '';
+	 
+	 $array = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1);
+        
+	 foreach($array as $roman => $value){
+	  $matches = intval($number/$value);
+	 
+	  $result .= str_repeat($roman,$matches);
+	 
+	  $number = $number % $value;
+	 }
+	 
+	 return $result;
+}
+
+	echo intTOroman(58);
+?>
